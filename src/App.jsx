3d8cef7d11f33180b705c521679733639453c0fa
Kieran Singh <kieransingh27@gmail.com>
2022-08-01 23:02:08 +1000

    Implemented auto scroll on page change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Header from "./Components/header.jsx";
 import MovieCard from "./Components/movieCard.jsx";
 import ColourButton from "./Components/colourButton.jsx";
+import { isDisabled } from "@testing-library/user-event/dist/utils/index.js";
 
 const App = () => {
   const coloursArray = ["Blue", "Red", "Yellow", "Green"];
+  const dummyRef = useRef(null);
 
   const [movieData, setMovieData] = useState(null);
   const [colour, setColour] = useState("red");
@@ -23,6 +25,7 @@ const App = () => {
       }
     };
     fetchData();
+    scrollIntoView();
   }, [colour, pageNumber]);
 
   useEffect(() => {
@@ -36,6 +39,7 @@ const App = () => {
   };
 
   const changePageNumber = (e) => {
+    console.log(e.target);
     const changeText = e.target.textContent;
     if (changeText === "Next") {
       setPageNumber((pageNumber) => pageNumber + 1);
@@ -46,14 +50,24 @@ const App = () => {
     }
   };
 
+  const scrollIntoView = () => {
+    dummyRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <>
       <Header />
       <div className="wrapper">
+        <div ref={dummyRef}></div>
         <div className="colour-select-container">
           {coloursArray.map((colour) => {
             return <ColourButton colour={colour} handleClick={handleClick} />;
           })}
+        </div>
+        <div className="movies-header">
+          <h2>Movies</h2>
+          <span>Colour: {colour}</span>
+          <span>Page: {pageNumber}</span>
         </div>
         <div className="movies-container">
           {movieData?.map((movie) => {
@@ -61,8 +75,8 @@ const App = () => {
           })}
         </div>
         <div className="pagination-container">
-          <button>Prev</button>
-          <button>Next</button>
+          <button onClick={changePageNumber}>Prev</button>
+          <button onClick={changePageNumber}>Next</button>
         </div>
       </div>
     </>
